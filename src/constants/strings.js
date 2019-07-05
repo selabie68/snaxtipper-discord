e885b2f153ca20afe4ec9b6ed9f84d2c66918bc6
Michael Smith <selabie68@gmail.com>
2019-07-05 16:46:35 +1000

    Updated help text to show server-specific prefix.

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,11 +1,11 @@
 const HELP_TEXT = `
 \`\`\`
-!tip <tagged_user> <amount> SNAX <memo> - Tips the tagged user e.g. !tip @anarcist 100 SNAX
-!deposit - Shows instructions for deposit including your unique id
-!withdraw <snax_account> <amount> SNAX <memo> - Withdraw from the bots smart contract to your SNAX account
-!balance - Displays your token balances
-!help - Shows this help text
-!donate <amount> SNAX <memo> - Donate to the awesome developer
+{prefix}tip <tagged_user> <amount> SNAX <memo> - Tips the tagged user e.g. {prefix}tip @anarcist 100 SNAX
+{prefix}deposit - Shows instructions for deposit including your unique id
+{prefix}withdraw <snax_account> <amount> SNAX <memo> - Withdraw from the bots smart contract to your SNAX account
+{prefix}balance - Displays your token balances
+{prefix}help - Shows this help text
+{prefix}donate <amount> SNAX <memo> - Donate to the awesome developer
 \`\`\`
 `
 
